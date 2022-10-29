b03b5471a6139b5709d35bb70b86d2e75afe2049
STARJAN J <starjanj007@gmail.com>
2022-10-29 23:46:16 +0530

    file modified

--- signup.java
+++ signup.java
@@ -11,37 +11,39 @@ public class signup
 
     public void getSignIn()
     {
+        System.out.println("            sign UP");
+
+        System.out.print("    Name            : " + "\t ");
+        name = sc.next();
+
+        System.out.print("    Age             : "+ "\t ");
+        age = sc.nextInt();
+
+        System.out.print("    Date of Birth   : "+ "\t ");
+        D_O_B = sc.next();
+
+        System.out.print("    Gender\n(Male/Female/Others): "+ "\t ");
+        gender = sc.next();
+        
         do
         {
-            System.out.println("            sign UP");
-
-            System.out.print("    Name            : " + "\t ");
-            name = sc.next();
-
-            System.out.print("    Age             : "+ "\t ");
-            age = sc.nextInt();
-
-            System.out.print("    Date of Birth   : "+ "\t ");
-            D_O_B = sc.next();
-
-            System.out.print("    Gender\n(Male/Female/Others): "+ "\t ");
-            gender = sc.next();
-
             System.out.print("    Password        : "+ "\t ");
             password = sc.nextInt();
 
             System.out.print("    Confirm Password: "+ "\t ");
             confirm_Password = sc.nextInt();
+
             if(password==confirm_Password)
             {
                 System.out.println("               |successfully sign up| \n     ");
+                break;
             }
             else
             {
                 System.out.println("                Re Enter password \n     ");
+                System.out.print("Enter 1 to Re Enter pasword : "+"\t");
+                input = sc.nextInt();
             }	
-            System.out.print("Enter 1 to Re Enter pasword : "+"\t");
-            input = sc.nextInt();
         }
         while (input==1);
 
